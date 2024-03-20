60982ac5534db734b6339593633dc711ffa40f62
root <root@Bugger485>
2024-03-20 20:03:12 +0900

    Login with google

---
+++ app/Http/Controllers/GoogleSocialiteController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+use Exception;
+use App\Models\User;
+
+class GoogleSocialiteController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function handleCallback()
+    {
+        try {
+
+            $user = Socialite::driver('google')->user();
+
+            $finduser = User::where('social_id', $user->id)->first();
+
+            if ($finduser) {
+
+                Auth::login($finduser, true);
+
+                return redirect('/login');
+            } else {
+                $existedUser = User::where('email', $user->email)->first();
+                if($existedUser) {
+                    $newUser = User::where('email', $user->email)->update([
+                        'social_id' => $user->id,
+                        'social_type' => 'google',
+                    ]);
+                    $newUser = $existedUser;
+                }else {
+                    $newUser = User::create(
+                        [
+                            'name' => $user->name,
+                            'email' => $user->email,
+                            'social_id' => $user->id,
+                            'social_type' => 'google',
+                            'password' => encrypt('my-google')
+                        ]
+                    );
+
+                }
+                Auth::login($newUser, true);
+
+                return redirect('/dashboard');
+            }
+        } catch (Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+}
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,32 @@
-import { defineConfig } from 'vite';
+import {
+    defineConfig
+} from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+import vue from '@vitejs/plugin-vue';
 export default defineConfig({
+    server: {
+        host: true,
+        hmr: {
+            host: 'localhost',
+        },
+        watch: {
+            usePolling: true,
+        },
+    },
     plugins: [
+        vue(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
+            input: ['resources/js/app.js'],
+            refresh: [
+                'app/Http/Controllers/**',
+                'resources/views/**',
+            ],
+        })
     ],
+    alias: {
+        '@': './resources/js',
+    },
+    optimizeDeps: {
+        exclude: ['js-big-decimal']
+    }
 });
